d8c73b7b89554e78d7fd8fd9c64dcad1ade32afe
John Gribbin <johnggribbin@gmail.com>
2019-08-09 18:45:01 -0400

    Add createSecret and decryptSecret to auth helpers

--- src/auth/helpers/index.js
+++ src/auth/helpers/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { ethers } from "ethers";
 import Web3 from "web3";
+import pbkdf2 from "pbkdf2";
+import aesjs from "aes-js";
 var web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
 
 export const createWallet = () => {
@@ -58,4 +60,60 @@ export const retrieveJwt = async ({ publicAddress, signedMessage }) => {
       return response.data;
     })
     .catch(error => console.log(error));
+};
+
+export const createSecret = (privateKey, password) => {
+  // create a salt of 10 random numbers
+  const salt = window.crypto.getRandomValues(new Uint32Array(1))[0].toString();
+  // use salt and users password to create key
+  const key = pbkdf2.pbkdf2Sync(password, salt, 1, 256 / 8, "sha512");
+  // create iv
+  const iv = window.crypto.getRandomValues(new Uint8Array(16));
+  // create 14 random numbers to be used as a pad on the private key
+  const pad = window.crypto
+    .getRandomValues(new Uint32Array(2))
+    .join("")
+    .substring(0, 14);
+  // add pad to private key and convert string to bytes
+  const privateKeyBytes = aesjs.utils.utf8.toBytes(`${pad}${privateKey}`);
+  // encryption utilizing key and iv
+  const aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
+  const encryptedPrivateKeyBytes = aesCbc.encrypt(privateKeyBytes);
+  // convert encrypted privateKey in bytes to a hex string
+  const encryptedPrivateKeyBytesAsHex = aesjs.utils.hex.fromBytes(
+    encryptedPrivateKeyBytes
+  );
+  // convert iv in bytes to a hex string
+  const ivAsHex = aesjs.utils.hex.fromBytes(iv);
+  // create secret string to email to user
+  const secret = `${salt},${ivAsHex},${encryptedPrivateKeyBytesAsHex}`;
+
+  return secret;
+};
+
+export const decryptSecret = (secret, password) => {
+  // split secret and obtain salt, iv and the encrypted privateKeyBytes as a hex string
+  const splitSecret = secret.split(",");
+  const salt = splitSecret[0];
+  const iv = aesjs.utils.hex.toBytes(splitSecret[1]);
+  const encryptedPrivateKeyBytesAsHex = splitSecret[2];
+  // use password and salt to generate key
+  const key = pbkdf2.pbkdf2Sync(password, salt, 1, 256 / 8, "sha512");
+  // convert to bytes
+  const encryptedPrivateKeyBytes = aesjs.utils.hex.toBytes(
+    encryptedPrivateKeyBytesAsHex
+  );
+  // decrypt the private key using key and iv
+  const aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
+  const decryptedPrivateKeyBytes = aesCbc.decrypt(encryptedPrivateKeyBytes);
+  // convert to string, which includes the pad that was added during encryption
+  const decryptedPrivateKeyWithPad = aesjs.utils.utf8.fromBytes(
+    decryptedPrivateKeyBytes
+  );
+  // remove pad
+  const privateKey = decryptedPrivateKeyWithPad.substring(
+    14,
+    decryptedPrivateKeyWithPad.length
+  );
+  return privateKey;
 };
