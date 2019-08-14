2a9e556e34327dd00cea287e19d62792af8f0b60
John Gribbin <johnggribbin@gmail.com>
2019-08-14 16:01:23 -0400

    Update placeholder JSON to reflect mikes latest designs

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -19,50 +19,68 @@ export default function Observations() {
   // }, [setUserData]);
 
   const renderRows = () => {
-    return userData.objects_observed.map(obj => (
-      <tr key={userData.objects_observed.indexOf(obj)}>
+    return data.objects_observed.map(obj => (
+      <tr key={data.objects_observed.indexOf(obj)}>
         <td>{obj.object_name}</td>
-        <td>
-          {obj.object_type + " " + obj.object_origin + " " + obj.object_purpose}
-        </td>
+        <td>{obj.object_origin}</td>
+        <td>{obj.object_primary_purpose}</td>
+        <td>{obj.object_type}</td>
+        <td>{obj.object_secondary_purpose}</td>
         <td>{obj.observation_count}</td>
+        <td>{obj.time_last_tracked}</td>
+        <td>{obj.username_last_tracked}</td>
       </tr>
     ));
   };
 
-  const convertTimestamp = timestamp => {
-    const dateObj = new Date(timestamp * 1000);
-    return `${dateObj.getMonth() + 1}/${dateObj.getDate()}`;
-  };
+  // const convertTimestamp = timestamp => {
+  //   const dateObj = new Date(timestamp * 1000);
+  //   return `${dateObj.getMonth() + 1}/${dateObj.getDate()}`;
+  // };
 
   const renderObservationHistory = () => {
-    return Object.keys(userData.observation_history.this_month).map(
-      timestamp => <p key={timestamp}>{convertTimestamp(timestamp)}</p>
-    );
+    return data.observation_history.map(observation => (
+      <tr key={data.observation_history.indexOf(observation)}>
+        <td>{observation.time_submitted}</td>
+        <td>{observation.time_submitted}</td>
+        <td>{observation.object_name}</td>
+        <td>{observation.right_ascension}</td>
+        <td>{observation.declanation}</td>
+        <td>{observation.brightness}</td>
+        <td>{observation.conditions}</td>
+      </tr>
+    ));
   };
 
   return (
     <div style={{ textAlign: "center" }}>
       <section style={{ margin: "1em" }}>
-        <h1 style={{ fontWeight: "bold" }}>{userData.user_name}</h1>
+        <h1 style={{ fontWeight: "bold" }}>{data.user_name}</h1>
         <img
           style={{ height: "auto", width: "120px" }}
-          src={userData.user_image}
+          src={data.user_image}
           alt="user avatar"
         />
-        <p>{userData.location}</p>
-        <p>Observations {userData.observation_count}</p>
-        <p>Objects {userData.objects_tracked}</p>
+        <p>Location = {data.location}</p>
+        <p>Objects Tracked = {data.objects_tracked}</p>
+        <p>Observation Count = {data.observation_count}</p>
+        <p>Avg. Quality Level = {data.avg_quality_level}</p>
+        <p>Bio = {data.bio}</p>
       </section>
 
       <section style={{ margin: "1em" }}>
-        <h1 style={{ fontWeight: "bold" }}>OBJECTS OBSERVED</h1>
+        <h1 style={{ fontWeight: "bold" }}>OBJECTS TRACKED</h1>
         <table>
           <thead>
             <tr>
-              <th>OBJECT</th>
-              <th>TRAITS</th>
-              <th>OBSERVATIONS</th>
+              <th>NAME</th>
+              <th>ORIGIN</th>
+              <th>PURPOSE1</th>
+              <th>TYPE</th>
+              <th>PURPOSE2</th>
+              <th>OBSERVATION COUNT</th>
+              <th>LAST TIME TRACKED</th>
+              <th>LAST USER TO TRACK</th>
             </tr>
           </thead>
           <tbody>{renderRows()}</tbody>
@@ -70,69 +88,96 @@ export default function Observations() {
       </section>
 
       <section style={{ margin: "1em" }}>
-        <h1 style={{ fontWeight: "bold" }}>STATS</h1>
-        {renderObservationHistory()}
-        <p>{userData.user_record.objects_tracked} OBJECTS</p>
-        <p>{userData.user_record.uncommon_birds_tracked} UNCOMMON BIRDS</p>
-        <p>{userData.user_record.rare_birds_tracked} RARE BIRDS</p>
-        <p>{userData.user_record.week_streak} WEEK STREAK</p>
+        <h1 style={{ fontWeight: "bold" }}>YOUR OBSERVATIONS</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>DATE</th>
+              <th>TIME</th>
+              <th>NAME</th>
+              <th>R.ASCENSION</th>
+              <th>DECLANATION</th>
+              <th>BRIGHTNESS</th>
+              <th>CONDITIONS</th>
+            </tr>
+          </thead>
+          <tbody>{renderObservationHistory()}</tbody>
+        </table>
       </section>
     </div>
   );
 }
 
-const userData = {
+const data = {
   user_name: "Scott_Tilley",
   user_image:
     "https://i.amz.mshcdn.com/KCJWkZNiwPyNXPcV0CN7yeL8G0A=/fit-in/1200x9600/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F784551%2F0e3defde-7d59-4d94-b094-51d187f930da.jpg",
   location: "Brixton, UK",
-  observation_count: "59K",
-  objects_tracked: 403,
+  observation_count: "59000",
+  objects_tracked: "403",
+  avg_quality_level: "65%",
   bio: "yada yada bio here...",
+  // objects they have tracked after we processes their observations (IODs submitted)
   objects_observed: [
     {
       object_name: "SL-27 R/B",
-      object_type: "Comms",
-      object_origin: "France",
-      object_purpose: "military",
-      time_spotted: "1550398277",
-      observation_count: "50"
+      object_origin: "france",
+      object_type: "satelitte",
+      object_primary_purpose: "military",
+      object_secondary_purpose: "communications",
+      observation_count: "50",
+      time_last_tracked: "1550398277",
+      address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+      username_last_tracked: "username"
     },
     {
       object_name: "SL-27 R/B",
-      object_type: "Comms",
-      object_origin: "France",
-      object_purpose: "military",
-      time_spotted: "1550398277",
-      observation_count: "50"
+      object_origin: "france",
+      object_type: "satelitte",
+      object_primary_purpose: "military",
+      object_secondary_purpose: "communications",
+      observation_count: "50",
+      time_last_tracked: "1550398277",
+      address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+      username_last_tracked: "username"
     },
     {
       object_name: "SL-27 R/B",
-      object_type: "Comms",
-      object_origin: "France",
-      object_purpose: "military",
-      time_spotted: "1550398277",
-      observation_count: "50"
+      object_origin: "france",
+      object_type: "satelitte",
+      object_primary_purpose: "military",
+      object_secondary_purpose: "communications",
+      observation_count: "50",
+      time_last_tracked: "1550398277",
+      address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+      username_last_tracked: "username"
     }
   ],
-  observation_history: {
-    this_month: {
-      "1550398277": 10,
-      "1550398274": 12,
-      "1550398272": 0,
-      "1550398271": 2
+  // essentially a full history of their IOD submissions
+  observation_history: [
+    {
+      time_submitted: "1550398277", // timestamp will be used to populate the date AND time fields in UI
+      object_name: "SL-27 R/B",
+      right_ascension: "03:42:45",
+      declanation: "12:32:53",
+      brightness: "1.4",
+      conditions: "fair"
     },
-    all_time: {
-      "1550398277": 10,
-      "1550398274": 12,
-      "1550398272": 0,
-      "1550398271": 2
+    {
+      time_submitted: "1550398277",
+      object_name: "SL-27 R/B",
+      right_ascension: "03:42:45",
+      declanation: "12:32:53",
+      brightness: "1.4",
+      conditions: "fair"
+    },
+    {
+      time_submitted: "1550398277",
+      object_name: "SL-27 R/B",
+      right_ascension: "03:42:45",
+      declanation: "12:32:53",
+      brightness: "1.4",
+      conditions: "fair"
     }
-  },
-  user_record: {
-    objects_tracked: "85",
-    uncommon_birds_tracked: "18",
-    rare_birds_tracked: "3",
-    week_streak: "15"
-  }
+  ]
 };
