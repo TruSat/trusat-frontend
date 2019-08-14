d50890bd16af29348f629ff21b88bca23971f62c
John Gribbin <johnggribbin@gmail.com>
2019-08-14 16:25:23 -0400

    Move LoginOptions components out of Catalog view and into Welcome view then add new styles

--- src/views/Catalog.js
+++ src/views/Catalog.js
@@ -1,49 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useAuthDispatch, useAuthState } from "../auth/auth-context";
-import { ethers } from "ethers";
-
-import LoginOptions from "../auth/components/LoginOptions";
-import AccountDetails from "../auth/components/AccountDetails";
+import React from "react";
 import CatalogTable from "../catalog/CatalogTable";
 
-export default function Home() {
-  const [isAppLoading, setIsAppLoading] = useState(true);
-  const { isAuth } = useAuthState();
-  const dispatch = useAuthDispatch();
-
-  useEffect(() => {
-    // get jwt from local storage, utilized for all login options
-    const retrieveJwt = () => {
-      if (localStorage.getItem("mvp-jwt")) {
-        const jwt = localStorage.getItem("mvp-jwt");
-        dispatch({ type: "SET_JWT", payload: jwt });
-        dispatch({ type: "AUTHENTICATED", payload: true });
-      }
-    };
-    // get burner wallet from local storage, utilized for burner login only
-    const retrieveWallet = () => {
-      if (localStorage.getItem("mvp-private-key")) {
-        const privateKey = localStorage.getItem("mvp-private-key");
-        const wallet = new ethers.Wallet(privateKey);
-
-        dispatch({ type: "SET_BURNER", payload: wallet });
-        dispatch({
-          type: "SET_ADDRESS",
-          payload: wallet.signingKey.address
-        });
-        dispatch({ type: "SET_AUTH_TYPE", payload: "burner" });
-      }
-    };
-    retrieveJwt();
-    retrieveWallet();
-    setIsAppLoading(false);
-  }, [dispatch]);
-
-  return (
-    <React.Fragment>
-      {isAppLoading ? null : <LoginOptions />}
-      {isAuth ? <AccountDetails /> : null}
-      <CatalogTable />
-    </React.Fragment>
-  );
+export default function Catalog() {
+  return <CatalogTable />;
 }
