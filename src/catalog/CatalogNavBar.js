bd3e0fe2ac095787da6eafb44338afd460dc9ba9
John Gribbin <johnggribbin@gmail.com>
2019-08-16 16:15:23 -0400

    Add NavBar component that will be used to filter the data rendered in CatalogTable

---
+++ src/catalog/CatalogNavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+export default function CatalogNavBar({ catalogFilter, setCatalogFilter }) {
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "space-around", margin: "1em" }}
+    >
+      <span
+        className={
+          catalogFilter === "priorities"
+            ? "nav-bar__link--highlight"
+            : "nav-bar__link--lowlight"
+        }
+        onClick={() => setCatalogFilter("priorities")}
+      >
+        PRIORITIES
+      </span>
+      <span
+        className={
+          catalogFilter === "undisclosed"
+            ? "nav-bar__link--highlight"
+            : "nav-bar__link--lowlight"
+        }
+        onClick={() => setCatalogFilter("undisclosed")}
+      >
+        UNDISCLOSED
+      </span>
+      <span
+        className={
+          catalogFilter === "debris"
+            ? "nav-bar__link--highlight"
+            : "nav-bar__link--lowlight"
+        }
+        onClick={() => setCatalogFilter("debris")}
+      >
+        DEBRIS
+      </span>
+      <span
+        className={
+          catalogFilter === "latest"
+            ? "nav-bar__link--highlight"
+            : "nav-bar__link--lowlight"
+        }
+        onClick={() => setCatalogFilter("latest")}
+      >
+        LATEST
+      </span>
+      <span
+        className={
+          catalogFilter === "all"
+            ? "nav-bar__link--highlight"
+            : "nav-bar__link--lowlight"
+        }
+        onClick={() => setCatalogFilter("all")}
+      >
+        ALL
+      </span>
+    </div>
+  );
+}
