e1365c0f51cd2aaee7754b28a3d918601a5f5506
John Gribbin <johnggribbin@gmail.com>
2019-08-16 16:14:41 -0400

    Add submit and get data buttons and import catalog filter navbar component

--- src/views/Catalog.js
+++ src/views/Catalog.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import CatalogNavBar from "../catalog/CatalogNavBar";
 import CatalogTable from "../catalog/CatalogTable";
 
 export default function Catalog() {
+  const [catalogFilter, setCatalogFilter] = useState("priorities");
+  console.log(`catalog filter = `, catalogFilter);
+
   return (
     <React.Fragment>
-      <NavLink to="/catalog/submit">Submit Observations</NavLink>
+      <NavLink style={{ color: "white" }} to="/catalog/submit">
+        <span
+          style={{
+            border: "1px solid #5F5F5F",
+            display: "inline-block",
+            padding: "0.5em"
+          }}
+        >
+          Submit data
+        </span>
+      </NavLink>
+      <span
+        style={{
+          border: "1px solid #5F5F5F",
+          display: "inline-block",
+          padding: "0.5em"
+        }}
+      >
+        Get data
+      </span>
+      <CatalogNavBar
+        catalogFilter={catalogFilter}
+        setCatalogFilter={setCatalogFilter}
+      />
+      {/* TODO - pass the catalog filter to the table for the api call */}
       <CatalogTable />
     </React.Fragment>
   );
