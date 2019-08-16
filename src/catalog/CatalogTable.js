871da183edaeeddee18defc503f1189e1a7aa957
John Gribbin <johnggribbin@gmail.com>
2019-08-16 15:17:12 -0400

    Add api to call to retrieve live data for table

--- src/catalog/CatalogTable.js
+++ src/catalog/CatalogTable.js
@@ -6,22 +6,22 @@ export default function PriorityObjectsTable() {
   const { jwt } = useAuthState();
   const [priorityObjects, setPriorityObjects] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       `http://ec2-18-222-251-120.us-east-2.compute.amazonaws.com:8080/mostRecent`
-  //     )
-  //     .then(result => {
-  //       // setPriorityObjects(result.data.community_observations);
-  //       console.log(result);
-  //     })
-  //     .catch(err => console.log(err));
-  // }, [setPriorityObjects]);
+  useEffect(() => {
+    axios
+      .get(
+        `http://ec2-18-222-251-120.us-east-2.compute.amazonaws.com:8080/priority`
+      )
+      .then(result => {
+        setPriorityObjects(result.data.community_observations);
+        console.log(result.data.community_observations);
+      })
+      .catch(err => console.log(err));
+  }, [setPriorityObjects]);
 
   const renderRows = () => {
-    return data.map(priObj => (
-      <tr key={data.indexOf(priObj)}>
-        <td>{data.indexOf(priObj) + 1}</td>
+    return priorityObjects.map(priObj => (
+      <tr key={priorityObjects.indexOf(priObj)}>
+        <td>{priorityObjects.indexOf(priObj) + 1}</td>
         <td>{priObj.object_name}</td>
         <td>{priObj.object_origin}</td>
         <td>{priObj.object_type}</td>
@@ -50,59 +50,59 @@ export default function PriorityObjectsTable() {
   );
 }
 
-const data = [
-  {
-    object_name: "sl-27",
-    object_origin: "russia",
-    object_type: "rocket body",
-    object_purpose: "comms",
-    time_last_tracked: "1565803593926", // timestamp
-    address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-    username_last_tracked: "username"
-  },
-  {
-    object_name: "abrixas rocket",
-    object_origin: "usa",
-    object_type: "satelitte",
-    object_purpose: "military",
-    time_last_tracked: "1565803593926",
-    address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-    username_last_tracked: "username"
-  },
-  {
-    object_name: "sl-27",
-    object_origin: "russia",
-    object_type: "rocket body",
-    object_purpose: "comms",
-    time_last_tracked: "1565803593926",
-    address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-    username_last_tracked: "username"
-  },
-  {
-    object_name: "abrixas rocket",
-    object_origin: "usa",
-    object_type: "satelitte",
-    object_purpose: "military",
-    time_last_tracked: "1565803593926",
-    address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-    username_last_tracked: "username"
-  },
-  {
-    object_name: "sl-27",
-    object_origin: "russia",
-    object_type: "rocket body",
-    object_purpose: "comms",
-    time_last_tracked: "1565803593926",
-    address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-    username_last_tracked: "username"
-  },
-  {
-    object_name: "abrixas rocket",
-    object_origin: "usa",
-    object_type: "satelitte",
-    object_purpose: "military",
-    time_last_tracked: "1565803593926",
-    address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-    username_last_tracked: "username"
-  }
-];
+// const data = [
+//   {
+//     object_name: "sl-27",
+//     object_origin: "russia",
+//     object_type: "rocket body",
+//     object_purpose: "comms",
+//     time_last_tracked: "1565803593926", // timestamp
+//     address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+//     username_last_tracked: "username"
+//   },
+//   {
+//     object_name: "abrixas rocket",
+//     object_origin: "usa",
+//     object_type: "satelitte",
+//     object_purpose: "military",
+//     time_last_tracked: "1565803593926",
+//     address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+//     username_last_tracked: "username"
+//   },
+//   {
+//     object_name: "sl-27",
+//     object_origin: "russia",
+//     object_type: "rocket body",
+//     object_purpose: "comms",
+//     time_last_tracked: "1565803593926",
+//     address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+//     username_last_tracked: "username"
+//   },
+//   {
+//     object_name: "abrixas rocket",
+//     object_origin: "usa",
+//     object_type: "satelitte",
+//     object_purpose: "military",
+//     time_last_tracked: "1565803593926",
+//     address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+//     username_last_tracked: "username"
+//   },
+//   {
+//     object_name: "sl-27",
+//     object_origin: "russia",
+//     object_type: "rocket body",
+//     object_purpose: "comms",
+//     time_last_tracked: "1565803593926",
+//     address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+//     username_last_tracked: "username"
+//   },
+//   {
+//     object_name: "abrixas rocket",
+//     object_origin: "usa",
+//     object_type: "satelitte",
+//     object_purpose: "military",
+//     time_last_tracked: "1565803593926",
+//     address_last_tracked: "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
+//     username_last_tracked: "username"
+//   }
+// ];
