a36907ae1883c258953a903665f501491d5f409a
John Gribbin <johnggribbin@gmail.com>
2019-08-28 15:45:40 -0400

    Remove unused code

--- src/object/components/HistoryYearDropdown.js
+++ src/object/components/HistoryYearDropdown.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import HistoryMonthDropdown from "./HistoryMonthDropdown";
 
 export default function HistoryTable({ noradNumber, objectOrigin }) {
