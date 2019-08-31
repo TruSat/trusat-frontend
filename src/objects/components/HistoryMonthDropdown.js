9d889e7df46668c742570b54343e5ef511bfe602
John Gribbin <johnggribbin@gmail.com>
2019-08-31 16:47:34 -0400

    Refactor code to open and close the accordion when a new month is chosen

--- src/objects/components/HistoryMonthDropdown.js
+++ src/objects/components/HistoryMonthDropdown.js
@@ -7,51 +7,45 @@ export default function HistoryMonthDropdown({
   yearNumber
 }) {
   const [monthChosen, setMonthChosen] = useState("");
-  const [showTable, setShowTable] = useState(false);
 
-  const renderMonthChoices = () => {
-    const months = [
-      { name: "January", number: 1 },
-      { name: "February", number: 2 },
-      { name: "March", number: 3 },
-      { name: "April", number: 4 },
-      { name: "May", number: 5 },
-      { name: "June", number: 6 },
-      { name: "July", number: 7 },
-      { name: "August", nunber: 8 },
-      { name: "September", number: 9 },
-      { name: "October", number: 10 },
-      { name: "November", number: 11 },
-      { name: "December", number: 12 }
-    ];
-
-    return months.map(month => {
-      return (
-        <div key={month.name} style={{ border: "1px solid blue" }}>
-          <h1
-            onClick={() => {
-              setMonthChosen(month.name);
-              setShowTable(!showTable);
-            }}
-          >
-            {month.name}
-          </h1>
-          {monthChosen === month.name && showTable === true ? (
-            <HistoryMonthTable
-              noradNumber={noradNumber}
-              objectOrigin={objectOrigin}
-              yearNumber={yearNumber}
-              monthNumber={month.number}
-            />
-          ) : null}
-        </div>
-      );
-    });
-  };
+  const months = [
+    { name: "January", number: 1 },
+    { name: "February", number: 2 },
+    { name: "March", number: 3 },
+    { name: "April", number: 4 },
+    { name: "May", number: 5 },
+    { name: "June", number: 6 },
+    { name: "July", number: 7 },
+    { name: "August", nunber: 8 },
+    { name: "September", number: 9 },
+    { name: "October", number: 10 },
+    { name: "November", number: 11 },
+    { name: "December", number: 12 }
+  ];
 
   return (
     <section style={{ display: "flex", flexDirection: "column" }}>
-      {renderMonthChoices()}
+      {months.map(month => {
+        return (
+          <div key={month.name} style={{ border: "1px solid blue" }}>
+            <h1
+              onClick={() => {
+                setMonthChosen(month.name);
+              }}
+            >
+              {month.name}
+            </h1>
+            {monthChosen === month.name ? (
+              <HistoryMonthTable
+                noradNumber={noradNumber}
+                objectOrigin={objectOrigin}
+                yearNumber={yearNumber}
+                monthNumber={month.number}
+              />
+            ) : null}
+          </div>
+        );
+      })}
     </section>
   );
 }
