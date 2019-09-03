e31dfef832404668ac5926abd78675f62d907f05
John Gribbin <johnggribbin@gmail.com>
2019-09-03 12:00:44 -0400

    Add filter description component that renders priorities description for now

---
+++ src/catalog/components/FilterDescription.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FilterDescription({ catalogFilter }) {
+  const filterDescriptions = {
+    priorities:
+      "This is a hitlist of satellites most in need of amateur observations.Higher priority sats include recent launches, changes in orbit, orlesser known orbits",
+    undisclosed:
+      "These are objects not found in most public catalogs... mike needs to complete this copy",
+    debris: "These are not full objects... mike needs to complete this copy",
+    latest: "Mike needs to complete this copy",
+    all: "Mike needs to complete this copy"
+  };
+
+  return <p>{filterDescriptions.priorities}</p>;
+}
--- src/views/Catalog.js
+++ src/views/Catalog.js
@@ -4,6 +4,7 @@ import Astriagraph from "../app/components/Astriagraph";
 import CatalogNavBar from "../catalog/components/CatalogNavBar";
 import CatalogTable from "../catalog/components/CatalogTable";
 import DownloadCatalogFilterTleButton from "../catalog/components/DownloadCatalogFilterTleButton";
+import FilterDescription from "../catalog/components/FilterDescription";
 
 export default function Catalog({ match }) {
   const catalogFilter = match.params.catalogFilter;
@@ -13,6 +14,8 @@ export default function Catalog({ match }) {
     <div className="catalog__wrapper">
       <Astriagraph />
       <h1 className="catalog__header">CATALOG</h1>
+      <CatalogNavBar catalogFilter={catalogFilter} setRange={setRange} />
+      <FilterDescription catalogFilter={catalogFilter} />
       <NavLink style={{ color: "white" }} to="/submit">
         <span
           style={{
@@ -29,7 +32,6 @@ export default function Catalog({ match }) {
         <DownloadCatalogFilterTleButton catalogFilter={catalogFilter} />
       ) : null}
 
-      <CatalogNavBar catalogFilter={catalogFilter} setRange={setRange} />
       <CatalogTable
         catalogFilter={catalogFilter}
         range={range}
