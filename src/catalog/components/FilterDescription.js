ab2362ec01024d3a9c7c2f6d46eebc8028e6d82a
John Gribbin <johnggribbin@gmail.com>
2019-09-03 15:24:11 -0400

    Update component to render different description for each catalog filter

--- src/catalog/components/FilterDescription.js
+++ src/catalog/components/FilterDescription.js
@@ -3,19 +3,25 @@ import React from "react";
 export default function FilterDescription({ catalogFilter }) {
   console.log(catalogFilter);
 
-  const filterDescriptions = {
-    priorities:
-      "This is a hitlist of satellites most in need of amateur observations.Higher priority sats include recent launches, changes in orbit, orlesser known orbits",
-    undisclosed:
-      "These are objects not found in most public catalogs... mike needs to complete this copy",
-    debris: "These are not full objects... mike needs to complete this copy",
-    latest: "Mike needs to complete this copy",
-    all: "Mike needs to complete this copy"
-  };
+  const filterDescriptions = [
+    {
+      descriptionName: "priorities",
+      descriptionCopy:
+        "This is a hitlist of satellites most in need of amateur observations.Higher priority sats include recent launches, changes in orbit, orlesser known orbits"
+    },
+    { descriptionName: "undisclosed", descriptionCopy: "bla bla undisclosed." },
+    { descriptionName: "debris", descriptionCopy: "bla bla debris." },
+    { descriptionName: "latest", descriptionCopy: "bla bla latest" },
+    { descriptionName: "all", descriptionCopy: "bla bla all." }
+  ];
 
-  return (
-    <p className="catalog__filter-description">
-      {filterDescriptions.catalogFilter}
-    </p>
-  );
+  return filterDescriptions.map(description => {
+    if (description.descriptionName === catalogFilter) {
+      return (
+        <p className="catalog__filter-description">
+          {description.descriptionCopy}
+        </p>
+      );
+    }
+  });
 }
