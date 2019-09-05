dcae65fa467b786f568457a41861c5803df1daef
John Gribbin <johnggribbin@gmail.com>
2019-09-05 16:30:25 -0400

    Update account settings with new button component

--- src/user/SecuritySettings.js
+++ src/user/SecuritySettings.js
@@ -5,19 +5,13 @@ export default function SecuritySettings() {
   return (
     <section className="security-settings__wrapper">
       <h2 className="security-settings__heading">DATA SECURITY</h2>
-      <p>
+      <p className="security-settings__info-text">
         Call to action about why you'd want to connect a wallet, and what
         features it will unlock for you
       </p>
 
       <NavLink to="/settings/security">
-        <span
-          style={{
-            border: "1px solid white",
-            display: "inline-block",
-            padding: "0.5em"
-          }}
-        >
+        <span className="app__white-button--small">
           Secure your account with MetaMask
         </span>
       </NavLink>
