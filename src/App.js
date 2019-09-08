8709a032a078e1e28f70f79cb2291f29c73cb197
John Gribbin <johnggribbin@gmail.com>
2019-09-08 17:11:35 -0400

    Move Auth and User provider components index.js and remove need for OnLoad component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { ethers } from "ethers";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { AuthProvider } from "./auth/auth-context";
-import { UserProvider } from "./user/user-context";
+import { useAuthDispatch } from "./auth/auth-context";
+import { useUserDispatch } from "./user/user-context";
 import { ObjectsProvider } from "./objects/objects-context";
 import NavBar from "./app/components/NavBar";
 import Catalog from "./views/Catalog";
@@ -16,32 +19,76 @@ import SignUp from "./views/SignUp";
 import ObjectInfo from "./views/ObjectInfo";
 import BurgerMenu from "./app/components/BurgerMenu";
 import MetamaskImport from "./views/MetamaskImport";
-import OnLoad from "./views/OnLoad";
 
 export default function App() {
+  const authDispatch = useAuthDispatch();
+  const userDispatch = useUserDispatch();
+
+  useEffect(() => {
+    // get jwt from local storage
+    // utilized for authentication and is decoded to return users ethereum address
+    const retrieveJwt = () => {
+      const jwt = localStorage.getItem("trusat-jwt");
+      authDispatch({ type: "SET_JWT", payload: jwt });
+      authDispatch({ type: "AUTHENTICATED", payload: true });
+
+      const { address } = jwt_decode(jwt);
+      userDispatch({ type: "SET_USER_ADDRESS", payload: address });
+
+      axios
+        .post(
+          `https://api.consensys.space:8080/profile`,
+          JSON.stringify({
+            jwt: jwt,
+            address: address
+          })
+        )
+        .then(result => {
+          userDispatch({ type: "SET_USER_DATA", payload: result.data });
+          userDispatch({ type: "SHOW_USER_PROFILE", payload: true });
+        })
+        .catch(err => console.log(err));
+    };
+    // private key for "burner wallet"
+    // will be called if user has submitted an observation without signing up to TruSat
+    const retrieveWallet = () => {
+      const privateKey = localStorage.getItem("trusat-private-key");
+      const wallet = new ethers.Wallet(privateKey);
+
+      authDispatch({ type: "SET_BURNER", payload: wallet });
+      userDispatch({
+        type: "SET_USER_ADDRESS",
+        payload: wallet.signingKey.address
+      });
+      authDispatch({ type: "SET_AUTH_TYPE", payload: "burner" });
+    };
+
+    if (localStorage.getItem("trusat-private-key")) {
+      retrieveWallet();
+    }
+
+    if (localStorage.getItem("trusat-jwt")) {
+      retrieveJwt();
+    }
+  }, [authDispatch, userDispatch]);
+
   return (
-    <AuthProvider>
-      <UserProvider>
-        <Router>
-          {/* TODO - create a better component to handle the "on Load" functions */}
-          <OnLoad />
-          <BurgerMenu left />
-          <NavBar />
-          <Route exact path="/" component={Welcome} />
-          <Route path="/catalog/:catalogFilter" component={Catalog} />
-          <Route path="/submit" component={Submit} />
-          <ObjectsProvider>
-            <Route path="/object/:number" component={ObjectInfo} />
-          </ObjectsProvider>
-          <Route path="/profile/:address" component={Profile} />
-          <Route exact path="/settings" component={AccountSettings} />
-          <Route path="/settings/metamask" component={MetamaskImport} />
-          <Route path="/about" component={About} />
-          <Route path="/how" component={HowTo} />
-          <Route path="/login" component={LogIn} />
-          <Route path="/signup" component={SignUp} />
-        </Router>
-      </UserProvider>
-    </AuthProvider>
+    <Router>
+      <BurgerMenu left />
+      <NavBar />
+      <Route exact path="/" component={Welcome} />
+      <Route path="/catalog/:catalogFilter" component={Catalog} />
+      <Route path="/submit" component={Submit} />
+      <ObjectsProvider>
+        <Route path="/object/:number" component={ObjectInfo} />
+      </ObjectsProvider>
+      <Route path="/profile/:address" component={Profile} />
+      <Route exact path="/settings" component={AccountSettings} />
+      <Route path="/settings/metamask" component={MetamaskImport} />
+      <Route path="/about" component={About} />
+      <Route path="/how" component={HowTo} />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/main.scss";
+import { AuthProvider } from "../src/auth/auth-context";
+import { UserProvider } from "../src/user/user-context";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AuthProvider>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </AuthProvider>,
+
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
