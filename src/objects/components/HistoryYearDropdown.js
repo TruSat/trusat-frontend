3dbd5f799d77bd1830b4768158784d4d530f66cb
John Gribbin <johnggribbin@gmail.com>
2019-09-09 15:32:56 -0400

    Update HistoryYearDropdown to perform with same functionality as month dropdown

--- src/objects/components/HistoryYearDropdown.js
+++ src/objects/components/HistoryYearDropdown.js
@@ -2,40 +2,38 @@ import React, { useState } from "react";
 import HistoryMonthDropdown from "./HistoryMonthDropdown";
 
 export default function HistoryTable() {
-  const [yearChosen, setYearChosen] = useState("2019");
-  const [showMonthDropdown, setShowMonthDropdown] = useState(true);
+  const [yearChosen, setYearChosen] = useState("");
 
-  const renderYearChoices = () => {
-    const years = [
-      "2019",
-      "2018",
-      "2017",
-      "2016",
-      "2015",
-      "2014",
-      "2013",
-      "2012",
-      "2011"
-    ];
+  const years = [
+    "2019",
+    "2018",
+    "2017",
+    "2016",
+    "2015",
+    "2014",
+    "2013",
+    "2012",
+    "2011"
+  ];
 
-    return years.map(year => {
-      return (
-        <div key={year} className="history-year-dropdown__row">
-          <h1
-            onClick={() => {
-              setYearChosen(year);
-              setShowMonthDropdown(!showMonthDropdown);
-            }}
-          >
-            <p className="history-year-dropdown__year-text">{year}</p>
-          </h1>
-          {yearChosen === year && showMonthDropdown === true ? (
-            <HistoryMonthDropdown yearNumber={yearChosen} />
-          ) : null}
-        </div>
-      );
-    });
-  };
-
-  return <section>{renderYearChoices()}</section>;
+  return (
+    <section className="history-year-dropdown">
+      {years.map(year => {
+        return (
+          <div key={year} className="history-year-dropdown__row">
+            <h1
+              onClick={() => {
+                setYearChosen(year);
+              }}
+            >
+              <p className="history-year-dropdown__year-text">{year}</p>
+            </h1>
+            {yearChosen === year ? (
+              <HistoryMonthDropdown yearNumber={yearChosen} />
+            ) : null}
+          </div>
+        );
+      })}
+    </section>
+  );
 }
