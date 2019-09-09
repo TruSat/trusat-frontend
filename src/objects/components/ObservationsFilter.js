ab62f79e978525cae90f2797e5c85c733636b684
John Gribbin <johnggribbin@gmail.com>
2019-09-09 15:42:22 -0400

    add conditional render for user sightings link

--- src/objects/components/ObservationsFilter.js
+++ src/objects/components/ObservationsFilter.js
@@ -1,8 +1,10 @@
 import React from "react";
 import DownloadObjectTleButton from "./DownloadObjectTleButton";
+import { useUserState } from "../../user/user-context";
 import { useObjectsState, useObjectsDispatch } from "../objects-context";
 
 export default function ObservationsFilter() {
+  const { userAddress } = useUserState();
   const { observationFilter } = useObjectsState();
   const objectsDispatch = useObjectsDispatch();
 
@@ -57,29 +59,31 @@ export default function ObservationsFilter() {
           </span>
         </div>
 
-        <div
-          className={
-            observationFilter === "mySightings"
-              ? "object-observations__nav-link-wrapper--highlight"
-              : "object-observations__nav-link-wrapper--lowlight"
-          }
-        >
-          <span
+        {userAddress ? (
+          <div
             className={
               observationFilter === "mySightings"
-                ? "object-observations__nav-link--highlight"
-                : "object-observations__nav-link--lowlight"
-            }
-            onClick={() =>
-              objectsDispatch({
-                type: "SET_OBSERVATION_FILTER",
-                payload: "mySightings"
-              })
+                ? "object-observations__nav-link-wrapper--highlight"
+                : "object-observations__nav-link-wrapper--lowlight"
             }
           >
-            My sightings
-          </span>
-        </div>
+            <span
+              className={
+                observationFilter === "mySightings"
+                  ? "object-observations__nav-link--highlight"
+                  : "object-observations__nav-link--lowlight"
+              }
+              onClick={() =>
+                objectsDispatch({
+                  type: "SET_OBSERVATION_FILTER",
+                  payload: "mySightings"
+                })
+              }
+            >
+              My sightings
+            </span>
+          </div>
+        ) : null}
       </div>
 
       <div className="object-observations__get-data-link-wrapper">
