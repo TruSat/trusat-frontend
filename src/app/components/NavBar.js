f61d79d932a7c044fb453be6e56910e0896333d6
John Gribbin <johnggribbin@gmail.com>
2019-09-19 09:31:20 -0400

    Update styling of nav bar to bring route links to center

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -23,106 +23,108 @@ function NavBar(props) {
         </div>
       </NavLink>
 
-      <div
-        className={
-          props.location.pathname === "/"
-            ? "nav-bar__link-wrapper--highlight"
-            : "nav-bar__link-wrapper--lowlight"
-        }
-      >
-        <NavLink
-          className={
-            props.location.pathname === "/"
-              ? "nav-bar__link--highlight"
-              : "nav-bar__link--lowlight"
-          }
-          to="/"
-        >
-          WELCOME
-        </NavLink>
-      </div>
-
-      <div
-        className={
-          props.location.pathname.includes("catalog") ||
-          props.location.pathname.includes("object") ||
-          props.location.pathname.includes("submit")
-            ? "nav-bar__link-wrapper--highlight"
-            : "nav-bar__link-wrapper--lowlight"
-        }
-      >
-        <NavLink
-          className={
-            props.location.pathname.includes("catalog") ||
-            props.location.pathname.includes("object") ||
-            props.location.pathname.includes("submit")
-              ? "nav-bar__link--highlight"
-              : "nav-bar__link--lowlight"
-          }
-          to={`/catalog/${catalogFilter}`}
-        >
-          CATALOG
-        </NavLink>
-      </div>
-
-      {/* // My Profile button only rendered when user is logged in */}
-      {userAddress ? (
+      <div className="nav-bar__route-link-wrapper">
         <div
           className={
-            props.location.pathname === `/profile/${userAddress}`
+            props.location.pathname === "/"
               ? "nav-bar__link-wrapper--highlight"
               : "nav-bar__link-wrapper--lowlight"
           }
         >
           <NavLink
             className={
-              props.location.pathname === `/profile/${userAddress}`
+              props.location.pathname === "/"
                 ? "nav-bar__link--highlight"
                 : "nav-bar__link--lowlight"
             }
-            to={`/profile/${userAddress}`}
+            to="/"
           >
-            MY PROFILE
+            WELCOME
           </NavLink>
         </div>
-      ) : null}
 
-      <div
-        className={
-          props.location.pathname === "/about"
-            ? "nav-bar__link-wrapper--highlight"
-            : "nav-bar__link-wrapper--lowlight"
-        }
-      >
-        <NavLink
+        <div
+          className={
+            props.location.pathname.includes("catalog") ||
+            // props.location.pathname.includes("object") ||
+            props.location.pathname.includes("submit")
+              ? "nav-bar__link-wrapper--highlight"
+              : "nav-bar__link-wrapper--lowlight"
+          }
+        >
+          <NavLink
+            className={
+              props.location.pathname.includes("catalog") ||
+              // props.location.pathname.includes("object") ||
+              props.location.pathname.includes("submit")
+                ? "nav-bar__link--highlight"
+                : "nav-bar__link--lowlight"
+            }
+            to={`/catalog/${catalogFilter}`}
+          >
+            CATALOG
+          </NavLink>
+        </div>
+
+        {/* // My Profile button only rendered when user is logged in */}
+        {userAddress ? (
+          <div
+            className={
+              props.location.pathname === `/profile/${userAddress}`
+                ? "nav-bar__link-wrapper--highlight"
+                : "nav-bar__link-wrapper--lowlight"
+            }
+          >
+            <NavLink
+              className={
+                props.location.pathname === `/profile/${userAddress}`
+                  ? "nav-bar__link--highlight"
+                  : "nav-bar__link--lowlight"
+              }
+              to={`/profile/${userAddress}`}
+            >
+              MY PROFILE
+            </NavLink>
+          </div>
+        ) : null}
+
+        <div
           className={
             props.location.pathname === "/about"
-              ? "nav-bar__link--highlight"
-              : "nav-bar__link--lowlight"
+              ? "nav-bar__link-wrapper--highlight"
+              : "nav-bar__link-wrapper--lowlight"
           }
-          to="/about"
         >
-          ABOUT
-        </NavLink>
-      </div>
+          <NavLink
+            className={
+              props.location.pathname === "/about"
+                ? "nav-bar__link--highlight"
+                : "nav-bar__link--lowlight"
+            }
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+        </div>
 
-      <div
-        className={
-          props.location.pathname === "/how"
-            ? "nav-bar__link-wrapper--highlight"
-            : "nav-bar__link-wrapper--lowlight"
-        }
-      >
-        <NavLink
+        <div
           className={
             props.location.pathname === "/how"
-              ? "nav-bar__link--highlight"
-              : "nav-bar__link--lowlight"
+              ? "nav-bar__link-wrapper--highlight"
+              : "nav-bar__link-wrapper--lowlight"
           }
-          to="/how"
         >
-          HOW TO
-        </NavLink>
+          <NavLink
+            className={
+              props.location.pathname === "/how"
+                ? "nav-bar__link--highlight"
+                : "nav-bar__link--lowlight"
+            }
+            to="/how"
+          >
+            <p>HOW TO</p>
+          </NavLink>
+        </div>
       </div>
 
       {/* Show Join button when user is not logged in */}
