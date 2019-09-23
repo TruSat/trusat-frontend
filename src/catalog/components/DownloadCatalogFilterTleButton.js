67ad49ea93bea007785444eee13d6a775d58e976
John Gribbin <johnggribbin@gmail.com>
2019-09-23 17:41:23 -0400

    Refactor downloadTLEButton to only hit endpoint onclick, not on load

--- src/catalog/components/DownloadCatalogFilterTleButton.js
+++ src/catalog/components/DownloadCatalogFilterTleButton.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, Fragment } from "react";
 import axios from "axios";
-import Spinner from "../../app/components/Spinner";
 import { useCatalogState, useCatalogDispatch } from "../catalog-context";
 import { API_ROOT } from "../../app/helpers";
 
@@ -10,45 +9,33 @@ export default function DownloadCatalogFilterTleButton({ catalogFilter }) {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [blobData, setBlobData] = useState("");
+  const [textFile, setTextFile] = useState(null);
 
-  useEffect(() => {
+  const fetchData = async () => {
     let didCancel = false;
 
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+    try {
+      const result = await axios(`${API_ROOT}/tle/trusat_${catalogFilter}.txt`);
 
-      try {
-        const result = await axios(
-          `${API_ROOT}/tle/trusat_${catalogFilter}.txt`
-        );
-
-        if (!didCancel) {
-          setBlobData(result.data);
-
-          catalogDispatch({
-            type: `SET_${catalogFilter.toUpperCase()}_TLE_DATA`,
-            payload: result.data
-          });
-        }
-      } catch (error) {
-        if (!didCancel) {
-          setIsError(true);
-        }
+      if (!didCancel) {
+        catalogDispatch({
+          type: `SET_${catalogFilter.toUpperCase()}_TLE_DATA`,
+          payload: result.data
+        });
+        return result.data;
       }
-      setIsLoading(false);
-    };
-
-    if (catalogFilter === "priorities" && prioritiesTleData.length !== 0) {
-      setBlobData(prioritiesTleData);
-    } else if (catalogFilter === "all" && allTleData.length !== 0) {
-      setBlobData(allTleData);
-    } else {
-      fetchData();
+    } catch (error) {
+      if (!didCancel) {
+        setIsError(true);
+      }
     }
-  }, [catalogFilter, prioritiesTleData, allTleData, catalogDispatch]);
+  };
 
-  const downloadTles = () => {
+  const downloadTles = async () => {
+    console.log(`hello`);
+
+    const blobData = await fetchData();
+
     let textFile = null;
 
     const dataToDownload = new Blob([blobData], { type: "text/plain" });
@@ -59,20 +46,26 @@ export default function DownloadCatalogFilterTleButton({ catalogFilter }) {
 
     textFile = window.URL.createObjectURL(dataToDownload);
 
-    return textFile;
+    setTextFile(textFile);
   };
 
-  return isError ? (
-    <p className="app__error-message">Something went wrong ...</p>
-  ) : isLoading ? (
-    <Spinner />
-  ) : (
-    <a
-      className="catalog__link"
-      href={downloadTles()}
-      download={`trusat_${catalogFilter}.txt`}
-    >
-      <span className="catalog__button catalog__get-data-button">Get data</span>
-    </a>
-  );
+  return catalogFilter === "priorities" || catalogFilter === "all" ? (
+    <Fragment>
+      <span
+        onClick={() => downloadTles()}
+        className="catalog__button catalog__get-data-button"
+      >
+        Get data
+      </span>
+      {textFile ? (
+        <a
+          className="catalog__link"
+          href={textFile}
+          download={`trusat_${catalogFilter}.txt`}
+        >
+          Download
+        </a>
+      ) : null}
+    </Fragment>
+  ) : null;
 }
