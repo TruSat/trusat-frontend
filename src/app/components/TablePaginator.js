1dfdc1c63f86f7a2e916bc5c2953bdd89eba6e7c
John Gribbin <johnggribbin@gmail.com>
2019-09-24 10:51:31 -0400

    Update styles of table paginator component

--- src/app/components/TablePaginator.js
+++ src/app/components/TablePaginator.js
@@ -2,36 +2,38 @@ import React from "react";
 
 export default function TablePaginator({ tableDataLength, range, setRange }) {
   return (
-    <div style={{ margin: "1em", textAlign: "center" }}>
-      <p>
-        {range.start + 1}-
-        {range.end > tableDataLength ? tableDataLength : range.end} of{" "}
-        {tableDataLength}
-      </p>
-      <button
-        onClick={() => {
-          if (range.start !== 0) {
-            setRange(currentRange => ({
-              start: currentRange.start - 10,
-              end: currentRange.end - 10
-            }));
-          }
-        }}
-      >
-        Left
-      </button>
-      <button
-        onClick={() => {
-          if (range.end < tableDataLength) {
-            setRange(currentRange => ({
-              start: currentRange.start + 10,
-              end: currentRange.end + 10
-            }));
-          }
-        }}
-      >
-        Right
-      </button>
+    <div className="table-paginator">
+      <div className="table-paginator__button-wrapper">
+        <button
+          onClick={() => {
+            if (range.start !== 0) {
+              setRange(currentRange => ({
+                start: currentRange.start - 10,
+                end: currentRange.end - 10
+              }));
+            }
+          }}
+        >
+          <p className="table-paginator__button-text">{`< Previous`}</p>
+        </button>
+        <p className="table-paginator__middle-text">
+          {range.start + 1}-
+          {range.end > tableDataLength ? tableDataLength : range.end} of{" "}
+          {tableDataLength}
+        </p>
+        <button
+          onClick={() => {
+            if (range.end < tableDataLength) {
+              setRange(currentRange => ({
+                start: currentRange.start + 10,
+                end: currentRange.end + 10
+              }));
+            }
+          }}
+        >
+          <p className="table-paginator__button-text">{`Next >`}</p>
+        </button>
+      </div>
     </div>
   );
 }
