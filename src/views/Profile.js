6c2b4c7b0925bca69aee3ddae43a8b9376a38fa1
John Gribbin <johnggribbin@gmail.com>
2019-09-26 16:23:06 -0400

    Update function to check for valid addresses

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -11,14 +11,8 @@ const isAddress = address => {
   // check if it has the basic requirements of an address
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
     return false;
-    // If it's ALL lowercase or ALL upppercase
   }
-  // else if (
-  //   /^(0x|0X)?[0-9a-f]{40}$/.test(address) ||
-  //   /^(0x|0X)?[0-9A-F]{40}$/.test(address)
-  // ) {
-  //   return true;
-  // }
+
   return true;
 };
 
