332dd8470ea51187834cbf5d48d2d04e45e48aaf
John Gribbin <johnggribbin@gmail.com>
2019-10-12 16:08:45 +0900

    Move sketch file for object badges to assets dir

--- src/app/components/ObjectBadge.js
+++ src/app/components/ObjectBadge.js
@@ -1,18 +1,16 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
-import sketch from "../components/sketch";
+import sketch from "../../assets/sketch.js";
 
 export default function ObjectBadge({ noradNumber, width, quality }) {
   return (
-    <div
-      className="catalog-table__object-data-wrapper__badgeWrapper"
-    >
-    <P5Wrapper
-      sketch={sketch}
-      noradNumber={noradNumber}
-      width={width}
-      quality={quality}
-    ></P5Wrapper>
+    <div className="catalog-table__object-data-wrapper__badgeWrapper">
+      <P5Wrapper
+        sketch={sketch}
+        noradNumber={noradNumber}
+        width={width}
+        quality={quality}
+      ></P5Wrapper>
     </div>
   );
 }
