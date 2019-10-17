a2da5a180f3d1c6b3c90127088b6596c9e04ed16
John Gribbin <johnggribbin@gmail.com>
2019-10-17 12:51:44 -0400

    Add footer social media icons to footer and update styles

---
+++ src/app/components/Footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import TwitterIcon from "../../assets/TwitterIcon.png";
+import FacebookIcon from "../../assets/FacebookIcon.png";
+import GithubIcon from "../../assets/GithubIcon.png";
+import DiscordIcon from "../../assets/DiscordIcon.svg";
+
+export default function Footer() {
+  return (
+    <div className="footer__wrapper">
+      <div className="footer__text-links-wrapper">
+        <NavLink className="app__nav-link footer__text-link" to="/terms">
+          Terms
+        </NavLink>
+        <NavLink className="app__nav-link" to="/privacy">
+          Privacy
+        </NavLink>
+      </div>
+      <div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://your_url_here.html"
+        >
+          <img class="footer__icon" src={TwitterIcon} alt="twitter"></img>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://your_url_here.html"
+        >
+          <img class="footer__icon" src={FacebookIcon} alt="facebook"></img>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://your_url_here.html"
+        >
+          <img class="footer__icon" src={GithubIcon} alt="github"></img>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://your_url_here.html"
+        >
+          <img class="footer__icon" src={DiscordIcon} alt="github"></img>
+        </a>
+      </div>
+    </div>
+  );
+}
