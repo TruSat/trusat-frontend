31886bb3401c7f8b36effcc293e99a5132d56b32
Michael Deal <michael.deal@consensys.net>
2019-10-17 22:07:35 -0400

    Update link section on about page

--- src/views/About.js
+++ src/views/About.js
@@ -13,118 +13,124 @@ export default function About() {
         </p>
 
         <div className="about__block-wrapper">
-
-        <div className="about__block-pair">
-          <div className="about__block--left">
-            <img
-              className="welcome__illustration welcome__illustration--left"
-              src={IllustrationPlaceholderWide}
-              alt="Illustration"
-            ></img>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <img
+                className="welcome__illustration welcome__illustration--left"
+                src={IllustrationPlaceholderWide}
+                alt="Illustration"
+              ></img>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                Citizen satellite trackers are the eyes of the TruSat system.
+              </p>
+            </div>
           </div>
-          <div className="about__block--right">
-            <p className="static-page__copy">
-              Citizen satellite trackers are the eyes of the TruSat system.
-            </p>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <img
+                className="welcome__illustration welcome__illustration--left"
+                src={IllustrationPlaceholderWide}
+                alt="Illustration"
+              ></img>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                The TruSat software merges observations of a satellite from around the world into a transparent record of its location.          
+              </p>
+            </div>
+          </div>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <img
+                className="welcome__illustration welcome__illustration--left"
+                src={IllustrationPlaceholderWide}
+                alt="Illustration"
+              ></img>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                Space sustainability advocates keep the system focused on the highest space sustainability priorities.  And they can use TruSat’s transparent record to foster accountability for sustainable orbital operations.
+              </p>
+            </div>
+          </div>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <img
+                className="welcome__illustration welcome__illustration--left"
+                src={IllustrationPlaceholderWide}
+                alt="Illustration"
+              ></img>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                The TruSat Open Source Community maintains and advances the TruSat software.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="about__block-pair">
-          <div className="about__block--left">
-            <img
-              className="welcome__illustration welcome__illustration--left"
-              src={IllustrationPlaceholderWide}
-              alt="Illustration"
-            ></img>
+
+        <div className="about__block-wrapper">
+          <h2 className="static-page__sub-header">LINKS</h2>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <NavLink className="app__nav-link static-page__link static-page__link--highlight" to="/faq">
+                Frequently asked questions
+              </NavLink>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                Learn about the ins and outs of TruSat.
+              </p>
+            </div>
           </div>
-          <div className="about__block--right">
-            <p className="static-page__copy">
-              The TruSat software merges observations of a satellite from around the world into a transparent record of its location.          
-            </p>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <NavLink className="app__nav-link static-page__link static-page__link--highlight" to="/whitepaper">
+                The TruSat white paper
+              </NavLink>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                A technical deep dive into how TruSat works.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="about__block-pair">
-          <div className="about__block--left">
-            <img
-              className="welcome__illustration welcome__illustration--left"
-              src={IllustrationPlaceholderWide}
-              alt="Illustration"
-            ></img>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <NavLink className="app__nav-link static-page__link static-page__link--highlight" to="/charter">
+                The TruSat Charter
+              </NavLink>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+              An outline of TruSat’s initial governance.
+              </p>
+            </div>
           </div>
-          <div className="about__block--right">
-            <p className="static-page__copy">
-              Space sustainability advocates keep the system focused on the highest space sustainability priorities.  And they can use TruSat’s transparent record to foster accountability for sustainable orbital operations.
-            </p>
+          <div className="about__block-pair">
+            <div className="about__block--left">
+              <a
+                className="static-page__link static-page__link--highlight"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://discordapp.com"
+              >
+                TruSat chat
+              </a>
+            </div>
+            <div className="about__block--right">
+              <p className="static-page__copy">
+                A Discord community chat to discuss the project and get help.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="about__block-pair">
-          <div className="about__block--left">
-            <img
-              className="welcome__illustration welcome__illustration--left"
-              src={IllustrationPlaceholderWide}
-              alt="Illustration"
-            ></img>
-          </div>
-          <div className="about__block--right">
-            <p className="static-page__copy">
-              The TruSat Open Source Community maintains and advances the TruSat software.
-            </p>
-          </div>
-        </div>
         </div>
 
 
 
-
-        <h2 className="static-page__sub-header">LINKS</h2>
-        <p className="static-page__copy">
-          <NavLink className="app__nav-link static-page__link" to="/whitepaper">
-            The TruSat white paper
-          </NavLink>{" "}
-          — A technical deep dive into how TruSat works.
-        </p>
-        <p className="static-page__copy">
-          <NavLink className="app__nav-link static-page__link" to="/charter">
-            The TruSat Charter
-          </NavLink>{" "}
-          — An outline of TruSat’s initial governance.
-        </p>
-        <p className="static-page__copy">
-          <NavLink className="app__nav-link static-page__link" to="/faq">
-            FAQ
-          </NavLink>{" "}
-          — Frequently asked questions
-        </p>
-        <p className="static-page__copy">
-          <a
-            className="static-page__link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://discordapp.com"
-          >
-            TruSat's community chat
-          </a>{" "}
-          — A Discord channel to discuss the project and get help.
-        </p>
-        <p className="static-page__copy">
-          Follow project updates on{" "}
-          <a
-            className="static-page__link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.facebook.com/PlanetaryResourcesInc/"
-          >
-            Facebook
-          </a>{" "}
-          and{" "}
-          <a
-            className="static-page__link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://twitter.com/planetaryrsrcs"
-          >
-            Twitter
-          </a>
-        </p>
+ 
         <div className="about__partners-wrapper">
           <h2 className="static-page__sub-header">PARTNERS</h2>
           <div className="about__partners-list">
@@ -211,6 +217,29 @@ export default function About() {
             Blockchain in Outer Space Might Just Work
           </a>
         </p>
+
+        <p className="static-page__copy">
+          Follow project updates on{" "}
+          <a
+            className="static-page__link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/PlanetaryResourcesInc/"
+          >
+            Facebook
+          </a>{" "}
+          and{" "}
+          <a
+            className="static-page__link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/planetaryrsrcs"
+          >
+            Twitter
+          </a>
+        </p>
+
+
       </section>
 
     </div>
