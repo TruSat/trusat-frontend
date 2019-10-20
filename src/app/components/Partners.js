02bb8bfb876112ac8cb6cb9945bba4494b126787
Michael Deal <michael.deal@consensys.net>
2019-10-20 10:05:32 -0400

    Update order of partner logos

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -8,13 +8,6 @@ export default function Partners() {
         <div className="welcome__partners-list__logo-wrapper">
           <img
             className="welcome__partners-list__logo"
-            src="https://trusat-assets.s3.amazonaws.com/partner_logo-consensys_space.png"
-            alt="Secure World Foundation Logo"
-          ></img>
-        </div>
-        <div className="welcome__partners-list__logo-wrapper">
-          <img
-            className="welcome__partners-list__logo"
             src="https://trusat-assets.s3.amazonaws.com/partner_logo-swf.png"
             alt="Secure World Foundation Logo"
           ></img>
@@ -30,6 +23,13 @@ export default function Partners() {
           <img
             className="welcome__partners-list__logo"
             src="https://trusat-assets.s3.amazonaws.com/partner_logo-moriba.png"
+            alt="Secure World Foundation Logo"
+          ></img>
+        </div>
+        <div className="welcome__partners-list__logo-wrapper">
+          <img
+            className="welcome__partners-list__logo"
+            src="https://trusat-assets.s3.amazonaws.com/partner_logo-consensys_space.png"
             alt="Secure World Foundation Logo"
           ></img>
         </div>
