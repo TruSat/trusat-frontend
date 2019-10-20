dff204e5143e6c2d657001ae71dd0e83caa216c4
John Gribbin <johnggribbin@gmail.com>
2019-10-19 21:31:38 -0400

    style a tag

--- src/views/SubscriptionConfirmed.js
+++ src/views/SubscriptionConfirmed.js
@@ -3,7 +3,6 @@ import TrusatLogoBig from "../assets/TrusatLogoBig.svg";
 import { NavLink } from "react-router-dom";
 import SocialIcons from "../app/components/SocialIcons";
 
-
 export default function SubscriptionConfirmed() {
   return (
     <div>
@@ -14,53 +13,56 @@ export default function SubscriptionConfirmed() {
             src={TrusatLogoBig}
             alt="Trusat logo"
           ></img>
-          <h2 className="join__trusat-tagline">OPEN SOURCE SPACE SUSTAINABILITY</h2>
+          <h2 className="join__trusat-tagline">
+            OPEN SOURCE SPACE SUSTAINABILITY
+          </h2>
         </div>
 
         <p className="subscription-confirmed__copy">Signup Confirmed</p>
-          <p className="static-page__copy">
-          Launched on October 21, 2019, you are among the earliest members this global open source community dedicated to preserving our spacefaring future.  TruSat is currently a prototype, with only a fraction of its planned functionality.  As an early member, you’ll have opportunities to shape TruSat at every stage.   
-          </p>
-          <p className="static-page__copy">
-          Please check your inbox for a welcome email.  You will receive updates about new features and opportunities to get involved in building both the software and community powering TruSat.
-          </p>
-          <p className="static-page__copy static-page__copy--bold">
+        <p className="static-page__copy">
+          Launched on October 21, 2019, you are among the earliest members this
+          global open source community dedicated to preserving our spacefaring
+          future. TruSat is currently a prototype, with only a fraction of its
+          planned functionality. As an early member, you’ll have opportunities
+          to shape TruSat at every stage.
+        </p>
+        <p className="static-page__copy">
+          Please check your inbox for a welcome email. You will receive updates
+          about new features and opportunities to get involved in building both
+          the software and community powering TruSat.
+        </p>
+        <p className="static-page__copy static-page__copy--bold">
           <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://discordapp.com/invite/MEyA2Ns"
-            >Join the conversation
-            </a>
-          </p>
-          <p className="static-page__copy">
-          Chat with your fellow TruSat community members and the people behind TruSat. Ask questions, share ideas.
-          </p>
-          <p className="static-page__copy static-page__copy--bold">
-            <NavLink
-              className="app__nav-link static-page__link"
-              to="/how"
-            >
-              Learn how to track satellites
-            </NavLink>
-          </p>
-          <p className="static-page__copy">
+            className="subscription-confirmed__link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://discordapp.com/invite/MEyA2Ns"
+          >
+            Join the conversation
+          </a>
+        </p>
+        <p className="static-page__copy">
+          Chat with your fellow TruSat community members and the people behind
+          TruSat. Ask questions, share ideas.
+        </p>
+        <p className="static-page__copy static-page__copy--bold">
+          <NavLink className="app__nav-link static-page__link" to="/how">
+            Learn how to track satellites
+          </NavLink>
+        </p>
+        <p className="static-page__copy">
           Try your hand at sat spotting and build your skills
-          </p>
-          <p className="static-page__copy static-page__copy--bold">
-          <NavLink
-              className="app__nav-link static-page__link"
-              to="/faq"
-            >
-              Learn more about TruSat
-            </NavLink>
-          </p>
-          <p className="static-page__copy">
+        </p>
+        <p className="static-page__copy static-page__copy--bold">
+          <NavLink className="app__nav-link static-page__link" to="/faq">
+            Learn more about TruSat
+          </NavLink>
+        </p>
+        <p className="static-page__copy">
           Who’s behind TruSat, why does it exist, how does it work?
-          </p>
-          <p className="static-page__copy static-page__copy--bold">
-          Follow us
-          </p>
-          <SocialIcons />
+        </p>
+        <p className="static-page__copy static-page__copy--bold">Follow us</p>
+        <SocialIcons />
       </div>
     </div>
   );
