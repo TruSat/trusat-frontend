78997167fd6d6b8fa425a8d79a0da4aa35ed308c
John Gribbin <johnggribbin@gmail.com>
2019-10-20 11:49:08 -0400

    Update src value for partners images

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-import PartnerSWF from "../../assets/partner_logo-swf.png"; //DELETE THIS LINE
-import PartnerSWISE from "../../assets/partner_logo-swise.png"; //DELETE THIS LINE
-import PartnerMoribaJah from "../../assets/partner_logo-moriba_jah.png"; //DELETE THIS LINE
-import PartnerConsenSysSpace from "../../assets/partner_logo-consensys_space.png"; //DELETE THIS LINE
-
-
 export default function Partners() {
   return (
     <div className="about__partners-wrapper">
@@ -19,11 +13,10 @@ export default function Partners() {
             href="https://swfound.org/"
           >
             <img
-            className="welcome__partners-list__logo"
-            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-swf.png"
-            src={PartnerSWF} //DELETE THIS LINE
-            alt="Secure World Foundation logo"
-          ></img>
+              className="welcome__partners-list__logo"
+              src="https://trusat-assets.s3.amazonaws.com/partner_logo-swf.png"
+              alt="Secure World Foundation logo"
+            ></img>
             Secure World Foundation
           </a>
         </div>
@@ -35,11 +28,10 @@ export default function Partners() {
             href="https://www.swise.org/"
           >
             <img
-            className="welcome__partners-list__logo"
-            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-swise.png"
-            src={PartnerSWISE} //DELETE THIS LINE
-            alt="Society of Women in Space Exploration logo"
-          ></img>
+              className="welcome__partners-list__logo"
+              src="https://trusat-assets.s3.amazonaws.com/partner_logo-swise.png"
+              alt="Society of Women in Space Exploration logo"
+            ></img>
             Society of Women in Space Exploration
           </a>
         </div>
@@ -51,11 +43,10 @@ export default function Partners() {
             href="https://www.linkedin.com/in/jahniverse/"
           >
             <img
-            className="welcome__partners-list__logo"
-            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-moriba.png"
-            src={PartnerMoribaJah} //DELETE THIS LINE
-            alt="Moriba Jah portrait"
-          ></img>
+              className="welcome__partners-list__logo"
+              src="https://trusat-assets.s3.amazonaws.com/partner_logo-moriba_jah.png"
+              alt="Moriba Jah portrait"
+            ></img>
             Moriba Jah
           </a>
         </div>
@@ -67,15 +58,13 @@ export default function Partners() {
             href="https://consensys.space"
           >
             <img
-            className="welcome__partners-list__logo"
-            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-consensys_space.png"
-            src={PartnerConsenSysSpace} //DELETE THIS LINE
-            alt="ConsenSys Space logo"
-          ></img>
+              className="welcome__partners-list__logo"
+              src="https://trusat-assets.s3.amazonaws.com/partner_logo-consensys_space.png"
+              alt="ConsenSys Space logo"
+            ></img>
             ConsenSys Space
           </a>
         </div>
-
       </div>
     </div>
   );
