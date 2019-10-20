2a1517378c935de3532424ce20b3b334d96bb4a0
Michael Deal <michael.deal@consensys.net>
2019-10-20 10:59:47 -0400

    Add temporary partner logo references

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import PartnerSWF from "../../assets/partner_logo-swf.png"; //DELETE THIS LINE
+import PartnerSWISE from "../../assets/partner_logo-swise.png"; //DELETE THIS LINE
+import PartnerMoribaJah from "../../assets/partner_logo-moriba_jah.png"; //DELETE THIS LINE
+import PartnerConsenSysSpace from "../../assets/partner_logo-consensys_space.png"; //DELETE THIS LINE
+
+
 export default function Partners() {
   return (
     <div className="about__partners-wrapper">
@@ -8,30 +14,38 @@ export default function Partners() {
         <div className="welcome__partners-list__logo-wrapper">
           <img
             className="welcome__partners-list__logo"
-            src="https://trusat-assets.s3.amazonaws.com/partner_logo-swf.png"
+            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-swf.png"
+            src={PartnerSWF} //DELETE THIS LINE
             alt="Secure World Foundation Logo"
           ></img>
+          <div></div>
         </div>
         <div className="welcome__partners-list__logo-wrapper">
           <img
             className="welcome__partners-list__logo"
-            src="https://trusat-assets.s3.amazonaws.com/partner_logo-swise.png"
+            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-swise.png"
+            src={PartnerSWISE} //DELETE THIS LINE
             alt="Secure World Foundation Logo"
           ></img>
+          <div></div>
         </div>
         <div className="welcome__partners-list__logo-wrapper">
           <img
             className="welcome__partners-list__logo"
-            src="https://trusat-assets.s3.amazonaws.com/partner_logo-moriba.png"
+            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-moriba.png"
+            src={PartnerMoribaJah} //DELETE THIS LINE
             alt="Secure World Foundation Logo"
           ></img>
+          <div></div>
         </div>
         <div className="welcome__partners-list__logo-wrapper">
           <img
             className="welcome__partners-list__logo"
-            src="https://trusat-assets.s3.amazonaws.com/partner_logo-consensys_space.png"
+            // src="https://trusat-assets.s3.amazonaws.com/partner_logo-consensys_space.png"
+            src={PartnerConsenSysSpace} //DELETE THIS LINE
             alt="Secure World Foundation Logo"
           ></img>
+          <div></div>
         </div>
       </div>
     </div>
