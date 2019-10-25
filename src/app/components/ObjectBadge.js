e902ecba90d9976b5227c74bf532bb673a33805e
John Gribbin <johnggribbin@gmail.com>
2019-10-25 13:21:41 -0400

    Remove unused props

--- src/app/components/ObjectBadge.js
+++ src/app/components/ObjectBadge.js
@@ -3,14 +3,13 @@ import P5Wrapper from "react-p5-wrapper";
 import SketchSmall from "../../assets/SketchSmall.js";
 import SketchLarge from "../../assets/SketchLarge.js";
 
-export default function ObjectBadge({ noradNumber, quality, size, addStyles }) {
+export default function ObjectBadge({ noradNumber, size, addStyles }) {
   return (
     // pass in small of large for width, see catalogTable and /object view to see difference
     <div className={`${addStyles}`}>
       <P5Wrapper
         sketch={size === "small" ? SketchSmall : SketchLarge}
         noradNumber={noradNumber}
-        // quality={quality}
       ></P5Wrapper>
     </div>
   );
