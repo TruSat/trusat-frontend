538c75f64ae6bf0be6ed830d26a8d8b0514db4a8
John Gribbin <johnggribbin@gmail.com>
2019-12-19 11:05:38 +1100

    Comment out fetch and straight axios calls and utilize new updated axios with cache hook

--- src/views/TestCookie.js
+++ src/views/TestCookie.js
@@ -3,6 +3,25 @@ import { useTrusatGetApi } from "../app/app-helpers";
 import axios from "axios";
 import Spinner from "../app/components/Spinner";
 
+export default function TestCookie() {
+  const [{ data, isLoading, errorMessage }, doFetch] = useTrusatGetApi();
+
+  useEffect(() => {
+    doFetch(`/cookieMonster`);
+  }, [doFetch]);
+
+  return (
+    <Fragment>
+      <div>This is a component for testing cookies</div>
+      {isLoading ? (
+        <Spinner />
+      ) : errorMessage ? (
+        <p className="app__error-message">{errorMessage}</p>
+      ) : null}
+    </Fragment>
+  );
+}
+
 // export default function TestCookie() {
 //   // const [{ data, isLoading, errorMessage }, doFetch] = useTrusatGetApi();
 //   const [isLoading, setIsLoading] = useState(false);
@@ -79,22 +98,3 @@ import Spinner from "../app/components/Spinner";
 //     </Fragment>
 //   );
 // }
-
-export default function TestCookie() {
-  const [{ data, isLoading, errorMessage }, doFetch] = useTrusatGetApi();
-
-  useEffect(() => {
-    doFetch(`/cookieMonster`);
-  }, [doFetch]);
-
-  return (
-    <Fragment>
-      <div>This is a component for testing cookies</div>
-      {isLoading ? (
-        <Spinner />
-      ) : errorMessage ? (
-        <p className="app__error-message">{errorMessage}</p>
-      ) : null}
-    </Fragment>
-  );
-}
