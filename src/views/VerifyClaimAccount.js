dc13052bd7a0cad32d374a74c89716580d6d2d78
John Gribbin <johnggribbin@gmail.com>
2020-01-17 16:15:37 -0500

    Remove unused commented out code

--- src/views/VerifyClaimAccount.js
+++ src/views/VerifyClaimAccount.js
@@ -80,7 +80,6 @@ export default function VerifyClaimAccount({ match }) {
           })
         );
         setIsSuccess(true);
-        //authDispatch({ type: "SET_JWT", payload: response.data.jwt });
         const { address } = await jwt_decode(response.data.jwt);
         authDispatch({ type: "SET_USER_ADDRESS", payload: address });
       } catch (error) {
