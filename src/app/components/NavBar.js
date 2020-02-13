b86e78b31d2d039176ca6c6fb10dadf50b69025b
John Gribbin <johnggribbin@gmail.com>
2020-02-13 16:40:37 -0500

    Update links to about and how to routes to instead link to the trusat-learn pages

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -136,19 +136,15 @@ function NavBar(props) {
               : "nav-bar__link-wrapper--lowlight"
           }
         >
-          <NavLink
-            className={
-              path === "/about"
-                ? "app__nav-link nav-bar__link--highlight"
-                : path === "/"
-                ? "app__nav-link nav-bar__link--lowlight--welcome"
-                : "app__nav-link nav-bar__link--lowlight"
-            }
-            to="/about"
+          <img className="app__nav__icon" src={IconGlobe} alt="icon"></img>
+          <a
+            className="app__nav-link nav-bar__link--lowlight--welcome"
+            href="https://learn.trusat.org/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img className="app__nav__icon" src={IconGlobe} alt="icon"></img>
             ABOUT
-          </NavLink>
+          </a>
         </div>
 
         <div
@@ -160,19 +156,15 @@ function NavBar(props) {
               : "nav-bar__link-wrapper--lowlight"
           }
         >
-          <NavLink
-            className={
-              path === "/how"
-                ? "app__nav-link nav-bar__link--highlight"
-                : path === "/"
-                ? "app__nav-link nav-bar__link--lowlight--welcome"
-                : "app__nav-link nav-bar__link--lowlight"
-            }
-            to="/how"
+          <img className="app__nav__icon" src={IconQuestion} alt="icon"></img>
+          <a
+            className="app__nav-link nav-bar__link--lowlight--welcome"
+            href="https://learn.trusat.org/docs/high-level-guide"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img className="app__nav__icon" src={IconQuestion} alt="icon"></img>
             HOW TO
-          </NavLink>
+          </a>
         </div>
       </div>
 
