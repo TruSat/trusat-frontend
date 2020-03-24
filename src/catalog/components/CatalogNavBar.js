3f9f4f764fed63493ac70b58ab7e63a0d16315b3
John Gribbin <johnggribbin@gmail.com>
2020-03-24 14:47:32 -0400

    Add data for celestrak categories to app helpers and update styles of catalog dropdown

--- src/catalog/components/CatalogNavBar.js
+++ src/catalog/components/CatalogNavBar.js
@@ -1,239 +1,30 @@
 import React, { useState } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import ReactGA from "react-ga";
+import { celestrakCategories } from "../../app/app-helpers";
 import IconArrowUp from "../../assets/icon-arrow-up.svg";
 import IconRocket from "../../assets/icon-rocket.svg";
 import IconLock from "../../assets/icon-lock.svg";
 import IconTrash from "../../assets/icon-trash.svg";
-
-const celestrakCategories = [
-  {
-    groupTitle: "Featured",
-    categories: [
-      { title: "100 Brightest", path: "100_brightest" },
-      { title: "Starlink", path: "starlink" },
-      {
-        title: "OneWeb",
-        path: "one_web"
-      },
-      {
-        title: "Active Sats",
-        path: "active_sats"
-      },
-      {
-        title: "Analyst sats",
-        path: "analyst_sats"
-      }
-    ]
-  },
-  {
-    groupTitle: "Debris",
-    categories: [
-      { title: "Indian ASAT Test", path: "indian_asat_test" },
-      { title: "FENGYUN 1C", path: "gengyun_1c" },
-      {
-        title: "IRIDIUM 33",
-        path: "iridium_33"
-      },
-      {
-        title: "COSMOS 2251",
-        path: "cosmos_2251"
-      }
-    ]
-  },
-  {
-    groupTitle: "Weather & Earth Resources",
-    categories: [
-      { title: "Weather", path: "weather" },
-      { title: "NOAA", path: "noaa" },
-      {
-        title: "GOES",
-        path: "goes"
-      },
-      {
-        title: "Earth Resources",
-        path: "earth_resources"
-      },
-      {
-        title: "Search & Rescue (SARSAT)",
-        path: "sarsat"
-      },
-      {
-        title: "Disaster Monitoring",
-        path: "disaster_monitoring"
-      },
-      {
-        title: "Tracking and Data Relay",
-        path: "tracking_and_data_relay"
-      },
-      {
-        title: "Satellite System (TDRSS)",
-        path: "tdrss"
-      },
-      {
-        title: "ARGOS Data Collection",
-        path: "argos"
-      },
-      {
-        title: "System",
-        path: "system"
-      },
-      {
-        title: "Planet",
-        path: "planet"
-      },
-      {
-        title: "Spire",
-        path: "spire"
-      }
-    ]
-  },
-  {
-    groupTitle: "Scientific",
-    categories: [
-      { title: "Space & Earth Science", path: "space_and_earth_science" },
-      { title: "Geodetic", path: "geodetic" },
-      {
-        title: "Engineering",
-        path: "engineering"
-      },
-      {
-        title: "Education",
-        path: "education"
-      }
-    ]
-  },
-  {
-    groupTitle: "Communications",
-    categories: [
-      { title: "Active Geosynchronous", path: "active_geosynchronous" },
-      { title: "GEO Protected Zone", path: "geo_protected_zone" },
-      {
-        title: "GEO Protected Zone Plus",
-        path: "geo_protected_zone_plus"
-      },
-      {
-        title: "Intelsat",
-        path: "intelsat"
-      },
-      {
-        title: "SES",
-        path: "ses"
-      },
-      {
-        title: "Iridium",
-        path: "iridium"
-      },
-      {
-        title: "Iridium Next",
-        path: "iridium_next"
-      },
-      {
-        title: "Orbcomm",
-        path: "orbcomm"
-      },
-      {
-        title: "Globalstar",
-        path: "globalstar"
-      },
-      {
-        title: "Amateur Radio",
-        path: "amateur_radio"
-      },
-      {
-        title: "Experimental",
-        path: "experimental"
-      },
-      {
-        title: "Other Comm",
-        path: "other_comm"
-      },
-      {
-        title: "SatNOGS",
-        path: "satnogs"
-      },
-      {
-        title: "Gorizant",
-        path: "gorizant"
-      },
-      {
-        title: "Raduga",
-        path: "raduga"
-      },
-      {
-        title: "Molniya",
-        path: "molniya"
-      }
-    ]
-  },
-  {
-    groupTitle: "Navigation",
-    categories: [
-      { title: "GPS Operational", path: "gps_operational" },
-      { title: "GLONASS Operational", path: "glonass_operational" },
-      {
-        title: "Galileo",
-        path: "galileo"
-      },
-      {
-        title: "Beidou",
-        path: "beidou"
-      },
-      {
-        title: "Satellite-Based Augmentation",
-        path: "satellite_based_augmentation"
-      },
-      {
-        title: "System",
-        path: "system_navigation"
-      },
-      {
-        title: "(WAAS/EGNOS/MSAS)",
-        path: "waas_egnos_msas"
-      },
-      {
-        title: "Navy Navigation Satellite",
-        path: "navy_navigation"
-      },
-      {
-        title: "System (NNSS)",
-        path: "nnss_system"
-      },
-      {
-        title: "Russian LEO Navigation",
-        path: "russian_leo_navigation"
-      }
-    ]
-  },
-  {
-    groupTitle: "Misc.",
-    categories: [
-      { title: "Miscellaneous Military", path: "misc_military" },
-      { title: "Radar Calibration", path: "radar_calibration" },
-      {
-        title: "CubeSats",
-        path: "cube_sats"
-      },
-      {
-        title: "Other",
-        path: "other"
-      }
-    ]
-  }
-];
 
 function CatalogNavBar({ catalogFilter, setRange, setDataStart, history }) {
   const [showMore, setShowMore] = useState(true);
 
   const renderCelestrakCategories = () => {
     return celestrakCategories.map(group => (
-      <div>
-        <h1>{group.groupTitle}</h1>
-        <div>
-          {group.categories.map(category => (
-            <NavLink to={`all/${category.path}`}>{category.title}</NavLink>
-          ))}
-        </div>
+      <div className="catalog-more-dropdown__group">
+        <h1 className="catalog-more-dropdown__group-header">
+          {group.groupTitle}
+        </h1>
+
+        {group.categories.map(category => (
+          <NavLink
+            to={`all/${category.path}`}
+            className="catalog-more-dropdown__link"
+          >
+            {category.title}
+          </NavLink>
+        ))}
       </div>
     ));
   };
@@ -327,7 +118,9 @@ function CatalogNavBar({ catalogFilter, setRange, setDataStart, history }) {
 
         <div
           className={
-            "catalog-nav-bar__link--lowlight catalog-nav-bar__more-link"
+            showMore
+              ? "catalog-more-dropdown__more-text"
+              : "catalog-nav-bar__link--lowlight"
           }
           onMouseEnter={() => setShowMore(true)}
           onMouseLeave={() => setShowMore(false)}
@@ -337,7 +130,7 @@ function CatalogNavBar({ catalogFilter, setRange, setDataStart, history }) {
       </section>
       {showMore ? (
         <section
-          className="catalog-nav-bar__more-dropdown"
+          className="catalog-more-dropdown"
           onMouseEnter={() => setShowMore(true)}
           onMouseLeave={() => setShowMore(false)}
         >
