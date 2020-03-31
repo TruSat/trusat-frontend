91bed4fa687f2074ad7424c78cf3b42651af9138
John Gribbin <johnggribbin@gmail.com>
2020-03-31 14:20:40 -0400

    Remove unused import and redundant code

--- src/app/components/Advisors.js
+++ src/app/components/Advisors.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-// import ReactGA from "react-ga";
 
 export default function Advisors() {
   return (
@@ -7,19 +6,6 @@ export default function Advisors() {
       <h2 className="static-page__sub-header">ADVISORS</h2>
       <div className="partners__wrapper">
         <div className="partners__logo-wrapper">
-          {/* <a
-            className="static-page__partner-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://consensys.space"
-            onClick={() => {
-              ReactGA.event({
-                category: "Outbound link",
-                action: `Clicked a partner link`,
-                label: `Clicked ConsenSys Space link`
-              });
-            }}
-          > */}
           <img
             className="advisors__logo"
             src="https://trusat-assets.s3.amazonaws.com/advisor_log-brad.jpg"
