9ce2fb72f1cbf3c32db898a917419700e103372c
John Gribbin <johnggribbin@gmail.com>
2020-04-01 16:34:24 -0400

    Update FilterDescription to render the group headers instead of the paths in the celestrak descriptions

--- src/catalog/components/FilterDescription.js
+++ src/catalog/components/FilterDescription.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-export default function FilterDescription({ catalogFilter }) {
+export default function FilterDescription({
+  catalogFilter,
+  celestrakCategories
+}) {
   const filterDescriptions = [
     {
       filter: "priorities",
@@ -29,7 +32,25 @@ export default function FilterDescription({ catalogFilter }) {
     description => description.filter === catalogFilter
   );
 
-  // if featured filter was chosen
+  console.log(celestrakCategories);
+
+  const getCelestrakCategoryName = () => {
+    if (celestrakCategories) {
+      // check group headers for a match first
+      // returns an array containing one object if a match is found
+      const groupHeaderMatch = celestrakCategories.filter(
+        group => group.groupHeader.path === catalogFilter
+      );
+      // return the "title" of the group header if the paths (url and API) match
+      if (groupHeaderMatch.length !== 0) {
+        return groupHeaderMatch[0].groupHeader.title;
+      } else {
+        return catalogFilter;
+      }
+    }
+  };
+
+  // if a NavBar "featured" filter was chosen
   return featuredDescription.length === 1 ? (
     // return detailed description of featured filter
     <p
@@ -39,9 +60,10 @@ export default function FilterDescription({ catalogFilter }) {
       {featuredDescription[0].copy}
     </p>
   ) : (
-    // otherwise return generic description of regular filter found in "more" dropdown
+    // otherwise return generic description of celestrak category filter found in "more" dropdown
     <p key={`${catalogFilter} copy`} className="catalog__filter-description">
-      {`All the objects classified as "${catalogFilter}" in the TruSat Catalog`}
+      All the objects classified as {getCelestrakCategoryName()} in the TruSat
+      Catalog
     </p>
   );
 }
