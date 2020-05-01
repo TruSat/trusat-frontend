1d975ae8c71fe71355170976c42ed4c9680e55a5
John Gribbin <johnggribbin@gmail.com>
2020-05-01 10:58:08 -0400

    Add sketch for badge in table and update styling of activity monitor components

--- src/app/components/ObjectBadge.js
+++ src/app/components/ObjectBadge.js
@@ -1,14 +1,23 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
-import SketchSmall from "../../assets/SketchSmall.js";
 import SketchLarge from "../../assets/SketchLarge.js";
+import SketchSmall from "../../assets/SketchSmall.js";
+import SketchExtraSmall from "../../assets/SketchExtraSmall.js";
 
 export default function ObjectBadge({ noradNumber, size, addStyles }) {
   return (
     // pass in small of large for width, see catalogTable and /object view to see difference
     <div className={`${addStyles}`}>
       <P5Wrapper
-        sketch={size === "small" ? SketchSmall : SketchLarge}
+        sketch={
+          size === "small"
+            ? SketchSmall
+            : size === "extra-small"
+            ? SketchExtraSmall
+            : size === "large"
+            ? SketchLarge
+            : null
+        }
         noradNumber={noradNumber}
       ></P5Wrapper>
     </div>
--- src/catalog/components/ActivityMonitor.js
+++ src/catalog/components/ActivityMonitor.js
@@ -1,22 +1,87 @@
 import React, { useEffect } from "react";
 import { useTrusatGetApi } from "../../app/app-helpers";
 import Spinner from "../../app/components/Spinner";
+import Satellite from "../../assets/Satellite.svg";
+import Eye from "../../assets/Eye.svg";
+import ObjectBadge from "../../app/components/ObjectBadge";
 
 const data = {
   total_sat_count: 4282,
   total_obs_count: 134811,
   // Should be 10 recents obs
   recent_obs: [
-    { sat_name: "1234", user_name: "space cadet1" },
-    { sat_name: "1234", user_name: "space cadet2" },
-    { sat_name: "1234", user_name: "space cadet3" },
-    { sat_name: "1234", user_name: "space cadet4" },
-    { sat_name: "1234", user_name: "space cadet5" },
-    { sat_name: "1234", user_name: "space cadet6" },
-    { sat_name: "1234", user_name: "space cadet7" },
-    { sat_name: "1234", user_name: "space cadet8" },
-    { sat_name: "1234", user_name: "space cadet9" },
-    { sat_name: "1234", user_name: "space cadet10" },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet1",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet2",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet3",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet3",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet3",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet4",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet5",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet6",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet7",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet8",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet9",
+      user_address: "0x123",
+    },
+    {
+      sat_name: "satname",
+      norad_number: "1234",
+      user_name: "space cadet10",
+      user_address: "0x123",
+    },
   ],
   current_month_obs_count: 1000,
   record_month_obs_count: 3000,
@@ -35,28 +100,47 @@ export default function ActivityMonitor() {
 
   return data ? (
     <div className="activity-monitor">
-      <section>
-        <h1>STATS</h1>
-        <div>
-          <p>{data.total_sat_count}</p>
-          <p>SATELLITES</p>
-        </div>
-        <div>
-          <p>{data.total_obs_count}</p>
-          <p>OBSERVATIONS</p>
+      <section className="activity-monitor__section activity-monitor__stats-section">
+        <h2 className="catalog__sub-header">STATS</h2>
+        <div className="activity-monitor__stats-wrapper">
+          <div className="activity-monitor__stat-wrapper">
+            <img src={Satellite} alt="sat" />
+            <div>
+              <p className="activity-monitor__stat-number">
+                {data.total_sat_count}
+              </p>
+              <p className="activity-monitor__stat-title">SATELLITES</p>
+            </div>
+          </div>
+          <div className="activity-monitor__stat-wrapper">
+            <img src={Eye} alt="eye" />
+            <div>
+              <p className="activity-monitor__stat-number">
+                {data.total_obs_count}
+              </p>
+              <p className="activity-monitor__stat-title">OBSERVATIONS</p>
+            </div>
+          </div>
         </div>
       </section>
-      <section>
-        <h1>RECENT OBSERVATIONS</h1>
+
+      <section className="activity-monitor__section">
+        <h2 className="catalog__sub-header">RECENT OBSERVATIONS</h2>
         {data.recent_obs.map((ob, postion) => (
-          <div key={postion}>
-            <p>{ob.sat_name}</p>
-            <p>{ob.user_name}</p>
+          <div key={postion} className="activity-monitor__recent-obs-row">
+            <ObjectBadge size="extra-small" noradNumber={ob.norad_number} />
+            <p className="activity-monitor__recent-obs-sat-name">
+              {ob.sat_name}
+            </p>
+            <p className="activity-monitor__recent-obs-user-name">
+              {ob.user_name}
+            </p>
           </div>
         ))}
       </section>
-      <section>
-        <h1>COMMUNITY GOAL</h1>
+
+      <section className="activity-monitor__section">
+        <h2 className="catalog__sub-header">COMMUNITY GOAL</h2>
         <p>
           {data.current_month_obs_count} OBSERVATIONS IN *Insert Month name*
         </p>
