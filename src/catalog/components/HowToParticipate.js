fb4fb5496462b9e9cfd6a79dfadcf33ce09c7acb
John Gribbin <johnggribbin@gmail.com>
2020-05-15 13:43:43 -0400

    Fix indentation with prettier

--- src/catalog/components/HowToParticipate.js
+++ src/catalog/components/HowToParticipate.js
@@ -7,14 +7,12 @@ export default function HowToParticipate({ catalogFilter }) {
     <div className="catalog__how-to-wrapper">
       <h2 className="catalog__how-to-text">WHAT IS THIS?</h2>
       <p className="catalog__sidebar-text">
-          This is an independent record of orbit predictions to help you spot satellites.{" "} 
-          <NavLink
-              className="app__nav-link catalog__link--lowlight"
-              to="/submit"
-            >
-              Submit
-            </NavLink>
-            {" "}your own observations to update the catalog.{" "}
+        This is an independent record of orbit predictions to help you spot
+        satellites.{" "}
+        <NavLink className="app__nav-link catalog__link--lowlight" to="/submit">
+          Submit
+        </NavLink>{" "}
+        your own observations to update the catalog.{" "}
         <a
           className="app__nav-link catalog__link--lowlight"
           href="https://learn.trusat.org/docs/start-here.html"
@@ -36,15 +34,15 @@ export default function HowToParticipate({ catalogFilter }) {
           </span>
         </NavLink>
         <div className="app__nav-link">
-            <a
-          className="catalog__button catalog__get-data-button"
-          href="https://keeptrack.space/?trusat"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          See orbits
-        </a>
-          </div>
+          <a
+            className="catalog__button catalog__get-data-button"
+            href="https://keeptrack.space/?trusat"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See orbits
+          </a>
+        </div>
       </div>
     </div>
   );
